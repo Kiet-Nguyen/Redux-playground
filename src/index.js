ca54c263be85df9df7ab5557452e1332552df489
Kiet <bunbothomngon@gmail.com>
2018-11-19 13:44:32 +0200

    redux basic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
